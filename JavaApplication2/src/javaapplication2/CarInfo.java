ccf6f69c0dc5eb9b26ea9b4d6430ea9f6a832cbb
ahmed2277 <ahmedzfahmi@gmail.com>
2024-04-13 20:20:15 +0300

    mereged the two CarInfo classes

--- JavaApplication2/src/javaapplication2/CarInfo.java
+++ JavaApplication2/src/javaapplication2/CarInfo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package javaapplication2;
 
 /**
@@ -9,36 +5,39 @@ package javaapplication2;
  * @author moeja
  */
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt
- * to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this
- * template
- */
-
 /**
  *
  * @author moeja
  */
 public class CarInfo {
-  private String color;
+  private final int carId;
   private String carType;
-  private int Credentials;
-  private String brand;
-  private int RentPerHour;
-  private int Model;
-  public boolean Avalability = true;
+  private String carBrand;
+  private String carModel;
+  private String carColor;
+  private String carCredentials;
+  private boolean isAvailable;
+  private double hourlyRate;
   private Customer renter;
 
-  public CarInfo(String color, String carType, int Credentials, String brand, int RentPerHour, int Model,
-      boolean Avalability) {
-    this.color = color;
+  private CarInfo(int carId, String carType, String carBrand, String carModel, String carColor,
+      String carCredentials, boolean isAvailable, double hourlyRate) {
+    this.carId = carId;
     this.carType = carType;
-    this.Credentials = Credentials;
-    this.brand = brand;
-    this.RentPerHour = RentPerHour;
-    this.Model = Model;
-    this.Avalability = Avalability;
+    this.carBrand = carBrand;
+    this.carModel = carModel;
+    this.carColor = carColor;
+    this.carCredentials = carCredentials;
+    this.isAvailable = isAvailable;
+    this.hourlyRate = hourlyRate;
+  }
+  // --------------------------------------------------------------------------
+  // --------------------------- getter and setters ---------------------------
+  // --------------------------------------------------------------------------
+  // the setters need to be changed later and be integrated with the db
+
+  public int getCarId() {
+    return carId;
   }
 
   public String getCarType() {
@@ -49,55 +48,40 @@ public class CarInfo {
     this.carType = carType;
   }
 
-  public int getCredentials() {
-    return Credentials;
+  public String getCredentials() {
+    return carCredentials;
   }
 
-  public void setCredentials(int Credentials) {
-    this.Credentials = Credentials;
+  public void setCredentials(String carCredentials) {
+    this.carCredentials = carCredentials;
   }
 
   public String getBrand() {
-    return brand;
+    return carBrand;
   }
 
-  public void setBrand(String brand) {
-    this.brand = brand;
+  public void setBrand(String carBrand) {
+    this.carBrand = carBrand;
   }
 
-  public int getRentPerHour() {
-    return RentPerHour;
+  public double getRentPerHour() {
+    return hourlyRate;
   }
 
-  public void setRentPerHour(int RentPerHour) {
-    this.RentPerHour = RentPerHour;
+  public void setRentPerHour(int hourlyRate) {
+    this.hourlyRate = hourlyRate;
   }
 
-  public int getModel() {
-    return Model;
+  public String getModel() {
+    return carModel;
   }
 
-  public void setModel(int Model) {
-    this.Model = Model;
+  public void setModel(String carModel) {
+    this.carModel = carModel;
   }
 
   public boolean isAvailable() {
-    return Avalability;
-  }
-
-  /*
-   * public boolean getAvalability(boolean Avalability){
-   * if(Avalability==false){
-   * return false;
-   * }
-   * else{
-   * return true;
-   * }
-   * }
-   */
-  public void setAvalability(boolean Avalability) {
-    this.Avalability = Avalability;
-    // updates avalablity of car
+    return isAvailable;
   }
 
   public Customer getRenter() {
@@ -110,4 +94,97 @@ public class CarInfo {
     // Assigning a car to a specific customer
   }
 
+  // --------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  // --------------------------------------------------------------------------
+  /*
+   * ==========================================
+   * I added this class because the constructor
+   * arguments are too long
+   * ==========================================
+   */
+
+  public static class CarInfoBuilder {
+    private final int carId;
+    private String carType;
+    private String carBrand;
+    private String carModel;
+    private String carColor;
+    private String carCredentials;
+    private boolean isAvailable;
+    private double hourlyRate;
+
+    /*
+     * This constructor is Mandatory because carId is final and we will have to pass
+     * an id every time we make a car
+     */
+
+    public CarInfoBuilder(int carId) {
+      this.carId = carId;
+    }
+
+    /*
+     * =========================================================
+     * here we return "this" so we can have chained method calls
+     * which is realy useful when we want to create a car,
+     * instead of typing a long constructor and mabey we don't
+     * have all the car info, we can just use the builder class
+     * with only the builder methods we need
+     * =========================================================
+     */
+    public CarInfoBuilder carType(String carType) {
+      this.carType = carType;
+      return this;
+    }
+
+    public CarInfoBuilder carBrand(String carBrand) {
+      this.carBrand = carBrand;
+      return this;
+    }
+
+    public CarInfoBuilder carModel(String carModel) {
+      this.carModel = carModel;
+      return this;
+    }
+
+    public CarInfoBuilder carColor(String carColor) {
+      this.carColor = carColor;
+      return this;
+    }
+
+    public CarInfoBuilder carCredentials(String carCredentials) {
+      this.carCredentials = carCredentials;
+      return this;
+    }
+
+    public CarInfoBuilder isAvailable(boolean isAvailable) {
+      this.isAvailable = isAvailable;
+      return this;
+    }
+
+    public CarInfoBuilder hourlyRate(double hourlyRate) {
+      this.hourlyRate = hourlyRate;
+      return this;
+    }
+
+    // after we finish we call the build function to call the constructor for us
+    public CarInfo build() {
+      return new CarInfo(carId, carType, carBrand, carModel, carColor,
+          carCredentials, isAvailable, hourlyRate);
+    }
+  }
+
+  // this will just print the car info
+  @Override
+  public String toString() {
+    return "Car Info:\n" +
+        "  Car Id: " + carId + "\n" +
+        "  Car Type: " + carType + "\n" +
+        "  Car Brand: " + carBrand + "\n" +
+        "  Car Model: " + carModel + "\n" +
+        "  Car Color: " + carColor + "\n" +
+        "  Car Credentials: " + carCredentials + "\n" +
+        "  Available: " + (isAvailable ? "Yes" : "No") + "\n" +
+        "  Hourly Rate: $" + String.format("%.2f", hourlyRate);
+  }
 }
